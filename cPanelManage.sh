e9c6275487aabde462e8975b510e7219294a867e
alleck <kaleb.lewis92@gmail.com>
2014-01-30 15:28:37 -0600

    Added cPanelManage.sh
    Added some simple disk reading commands

---
+++ cPanelManage.sh
@@ -0,0 +1,10 @@
+##############################
+# Finding disk usage folders #
+##############################
+
+#Find the size folders located in the directory you are in.
+du -sh
+
+#Find and sort the size folders located in the directory you are in.
+du -sh | sort -n
+
