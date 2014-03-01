0ce203c80743a317546cc35784f7535ed19433fa
alleck <kaleb.lewis92@gmail.com>
2014-03-01 17:03:41 -0600

    Update cPanelManage.sh

--- cPanelManage.sh
+++ cPanelManage.sh
@@ -8,3 +8,5 @@ du -sh *
 #Find and sort the size folders located in the directory you are in.
 du -sh * | sort -n
 
+#Test load time on a site from command line (Can add "watch -n #" to it and have a recursive output)
+(time wget -p --no-cache --delete-after discsunlimited.net -q) 2>&1 | awk '/real/ {print $2}' | awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }'
