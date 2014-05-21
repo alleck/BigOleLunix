669e430c52db2b7c2374033ac957bbfa38d14d77
alleck <kaleb.lewis92@gmail.com>
2014-05-21 18:13:24 -0500

    Update cPanelManage.sh

--- cPanelManage.sh
+++ cPanelManage.sh
@@ -10,3 +10,6 @@ du -sh * | sort -n
 
 #Test load time on a site from command line (Can add "watch -n #" to it and have a recursive output)
 (time wget -p --no-cache --delete-after discsunlimited.net -q) 2>&1 | awk '/real/ {print $2}' | awk '{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }'
+
+#cPanel mail log location
+/var/log/exim_main
